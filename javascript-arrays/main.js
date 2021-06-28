47ee8455d9eb189bb6537e291c306a8120fe5505
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-06-28 16:50:34 -0700

    Completed JavaScript Arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,15 @@
+var colors = ['red', 'white', 'blue'];
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log('American is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+console.log(colors);
+
+var students = ['DaVinci', 'Picasso', 'Van Gogh', 'Michealangelo'];
+var numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' in the class.');
+var lastStudent = students[numberOfStudents - 1];
+console.log('The last student is: ' + lastStudent);
+console.log(students);

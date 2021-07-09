6edfbcb0b972fcb4422212e94ead5cc8f1d501b7
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-09 10:56:28 -0700

    Removed console Logs

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,7 +1,5 @@
 var taskList = document.querySelector('.task-list');
 function done(event) {
-  console.log(event.target);
-  console.log(event.target.tagName);
   if (event.target.tagName === 'BUTTON') {
     var finished = event.target.closest('.task-list-item');
     finished.remove();

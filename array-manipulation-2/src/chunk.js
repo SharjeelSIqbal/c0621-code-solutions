a8a7c381d70f174c5494b9637af1a0b9db7224f7
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-19 13:00:48 -0700

    Completed includes.js

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,13 +1,1 @@
 /* exported chunk */
-function chunk(array, size) {
-  var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    var value = array[i];
-    if (i < size) {
-      if (value || typeof value === 'boolean') {
-        newArray.push(value);
-      }
-    }
-  }
-  return newArray;
-}
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,12 @@
 /* exported includes */
+function includes(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    var truthyValue = array[i];
+    if (truthyValue || typeof truthyValue === 'boolean') {
+      if (truthyValue === value) {
+        return true;
+      }
+    }
+  }
+  return false;
+}

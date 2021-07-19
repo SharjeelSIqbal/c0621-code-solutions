d8dd691c67df36e0b5b48d1d9aab45b70805112c
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-19 15:03:20 -0700

    Working on Chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,11 @@
 /* exported chunk */
+function chunk(array, size) {
+  var newArray = [];
+  var chunkArray = [];
+  for (var i = size; i < array.length; i++) {
+    for (i = 0; i < size; i++) {
+      newArray.push(array[i]);
+    } chunkArray.push(newArray);
+  }
+  return chunkArray;
+}

a0aee821a3b4e2b64c30d5c136b030064c578a61
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-20 16:29:53 -0700

    Work in progress JS Bank Challenge

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,29 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && balance % 1 === 0) {
+    var account = new Account(this.nextAccountNumber, holder);
+    this.accounts.push(account);
+    account.deposit(balance);
+    this.nextAccountNumber += 1;
+    return account.number;
+  }
+  return null;
+};
+
+Bank.prototype.getAccount = function (number) {
+  var account = this.accounts[number];
+  if (!this.accounts.includes(account)) {
+    return null;
+  }
+  var newAccount = new Account(account.number, account.holder);
+
+  return newAccount;
+  //   if (this.accounts.includes[number]) {
+  //   return this.account[number];
+  // }
+};

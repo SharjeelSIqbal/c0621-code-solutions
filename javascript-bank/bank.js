53279d11a4ae528cec963812ac3c459c067e0dce
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-20 19:39:32 -0700

    Still working on it

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -9,6 +9,7 @@ Bank.prototype.openAccount = function (holder, balance) {
     var account = new Account(this.nextAccountNumber, holder);
     this.accounts.push(account);
     account.deposit(balance);
+    account.number = this.nextAccountNumber;
     this.nextAccountNumber += 1;
     return account.number;
   }
@@ -17,13 +18,27 @@ Bank.prototype.openAccount = function (holder, balance) {
 
 Bank.prototype.getAccount = function (number) {
   var account = this.accounts[number];
+
   if (!this.accounts.includes(account)) {
     return null;
   }
   var newAccount = new Account(account.number, account.holder);
 
   return newAccount;
-  //   if (this.accounts.includes[number]) {
-  //   return this.account[number];
-  // }
+};
+
+Bank.prototype.getTotalAssets = function () {
+  if (this.accounts.length === 0) {
+    return 0;
+  }
+  var total = 0;
+  var accounts = this.accounts;
+
+  for (var i = 0; i < accounts.length; i++) {
+    // console.log(accounts[i]);
+    total += accounts[i].getBalance();
+    // console.log(total);
+  }
+  return total;
+
 };

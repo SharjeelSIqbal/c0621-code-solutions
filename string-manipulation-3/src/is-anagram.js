7d49e55f43a5f43bd4b69d7316ca889e63b30fc6
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-24 18:54:16 +0000

    Completed is-anagram and reverse-words, working on title-case

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,4 +1,18 @@
 /* exported isAnagram */
 function isAnagram(firstString, secondString) {
-  return false;
+  var newFirstString = [];
+  var newSecondString = [];
+  for (var i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== ' ') {
+      newFirstString.push(firstString[i]);
+    }
+  }
+  for (i = 0; i < secondString.length; i++) {
+    if (secondString[i] !== ' ') {
+      newSecondString.push(secondString[i]);
+    }
+  }
+  newFirstString = newFirstString.sort().join('');
+  newSecondString = newSecondString.sort().join('');
+  return newFirstString === newSecondString;
 }
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,16 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var wordArr = [];
+  string = string.split(' ');
+  for (var i = 0; i < string.length; i++) {
+    var newWords = '';
+    for (var j = string[i].length - 1; j >= 0; j--) {
+      newWords += string[i][j];
+
+      if (newWords.length === string[i].length) {
+        wordArr.push(newWords);
+      }
+    }
+  }
+  return wordArr.join(' ');
+}

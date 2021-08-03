e4da9ecfc48f3144e40dcfce65d148c581f610dc
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-03 23:24:19 +0000

    In progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,23 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// var cards = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+// var valueTypes = ['Spades', 'Clovers', 'Diamond', 'Heart'];
+
+// function Player(name) {
+//   this.name = name;
+//   var cardsGiven = [];
+//   var score = 0;
+//   var winner = false;
+// }
+
+// function playGame(name1, name2, name3, name4) {
+//   var player1 = new Player(name1);
+//   var player2 = new Player(name2);
+//   var player3 = new Player(name3);
+//   var player4 = new Player(name4);
+
+// }
+
+// function randomizeCards() {
+//   var eightRandom = [];
+
+// }

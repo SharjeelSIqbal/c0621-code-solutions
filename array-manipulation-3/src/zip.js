c4dd149f5916acabc95c2da13beea3d5abf1fe37
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-03 21:44:41 +0000

    Completed flatten method by checking which array indexs weren't an array itself then storing them inside a variable and then looping through the arrays within arrays and storing them inside the same variable, for the zip function I created a loop and checked if the array length was less than the loop to store all second variablesarray values

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,14 @@
 /* exported zip */
+function zip(first, second) {
+  var newArray = [];
+  var fullArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (i < first.length && i < second.length) {
+      newArray.push(first[i]);
+      newArray.push(second[i]);
+      fullArray.push(newArray);
+      newArray = [];
+    }
+  }
+  return fullArray;
+}

d86a36e0116a8ff5e30b4db72163e42ed47a36d9
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-04 17:40:56 +0000

    Finished last string alorithim, by hard-coding specific words prepositions, conjuctions, etc and focusing on uppercasing where it's necessary

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,28 @@
 /* exported titleCase */
+function titleCase(string) {
+  var shortWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+  string = string.split(' ');
+  for (var i = 0; i < string.length; i++) {
+
+    if (string[i] === 'api') {
+      string[i] = string[i].toUpperCase();
+    }
+    if (string[i].toLowerCase().includes('javascript')) {
+      string[i] = string[i][0].toUpperCase() + string[i].substring(1, 4) + string[i][4].toUpperCase() + string[i].substring(5);
+    }
+    if (!shortWords.includes(string[i]) || string[i] === string[0]) {
+      string[i] = string[i][0].toUpperCase() + string[i].substring(1);
+    }
+    if (string[i].includes(':')) {
+      string[i + 1] = string[i + 1][0].toUpperCase() + string[i + 1].substring(1);
+    }
+    if (string[i].includes('-')) {
+      var newString = string[i].split('-');
+      newString[1] = newString[1][0].toUpperCase() + newString[1].substring(1);
+      newString = newString.join('-');
+      string[i] = newString;
+    }
+  }
+  string = string.join(' ');
+  return string;
+}

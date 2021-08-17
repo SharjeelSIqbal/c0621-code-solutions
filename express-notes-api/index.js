a88bbd621c8416ae68bae620491a75efcb69322b
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-17 17:03:23 +0000

    Making final touches on the program

---
+++ express-notes-api/index.js
@@ -0,0 +1,96 @@
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 3000;
+
+app.use(express.json());
+app.get('/api/notes', (req, res) => {
+  const arr = [];
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    for (const id in data.notes) {
+      arr.push(data.notes[id]);
+    }
+    res.status(200).send(arr);
+  });
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    if (req.params.id < 0) {
+      res.status(400).send('Error: Number must be positive');
+    }
+    if (data.notes[req.params.id]) {
+      res.status(200).send(data.notes[req.params.id]);
+    } else {
+      res.status(404).send(`Error: ${req.params.id} cannot be found`);
+    }
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  if (Object.keys(req.body)[0] === 'content') {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      data = JSON.parse(data);
+      data.notes[data.nextId] = req.body;
+      data.notes[data.nextId].id = data.nextId;
+      data.nextId++;
+      fs.writeFile('./data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+        if (err) throw err;
+        res.status(200).send(req.body);
+      });
+    });
+  } else {
+    res.status(400).send('Error: Content is a required field');
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    if (req.params.id < 0) {
+      res.status(400).send('Error: ID must be a positive integer');
+    }
+    if (data.notes[req.params.id]) {
+      delete data.notes[req.params.id];
+      fs.writeFile('./data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+        if (err) throw err;
+      });
+      res.sendStatus(200);
+    } else {
+      res.status(404).send(`Error ${req.params.id} request not found`);
+    }
+  });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    if (req.params.id < 0) {
+      res.status(400).send('Error: ID must be a positive integer');
+    }
+    if (data.notes[req.params.id]) {
+      if (Object.keys(req.body)[0] === 'content') {
+        data.notes[req.params.id] = req.body;
+        data.notes[req.params.id].id = req.params.id;
+        fs.writeFile('./data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+          if (err) throw err;
+        });
+        res.status(200).send(req.body);
+      } else {
+        res.status(400).send('Error: "content" is a required field.');
+      }
+    } else {
+      res.status(404).send(`Error ${req.params.id} request not found`);
+    }
+  });
+});
+app.listen(port, () => {
+});

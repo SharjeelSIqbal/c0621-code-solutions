a27c9ca839cfb2ef481c1b9c20153b09591dee51
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-19 17:50:43 +0000

    Error has been removed and replaced with just the context

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -3,9 +3,5 @@ const takeAChance = require('./take-a-chance');
 const promise1 = takeAChance('Sharjeel');
 
 promise1
-  .then(value => {
-    console.log(value);
-  })
-  .catch(reason => {
-    console.error(`${reason}`);
-  });
+  .then(value => console.log(value))
+  .catch(reason => console.error(reason.message));

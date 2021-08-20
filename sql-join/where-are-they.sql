7e4cb6c889b98b7fc4a8c117161ec5b3574bee07
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-20 20:24:45 +0000

    Made changes to big spenders and added the amount, and for where-are-they removed customer names and only listed their addresses

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -1,8 +1,4 @@
-select "customers"."firstName",
-"customers"."lastName",
-"payments"."amount",
-"payments"."customerId",
-"addresses"."line1",
+select "addresses"."line1",
 "addresses"."district",
 "cities"."name" as "city",
 "countries"."name" as "country"

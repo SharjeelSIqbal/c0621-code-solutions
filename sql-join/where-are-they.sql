41da10a5f3ed55f4d0c4a44f26edff06786dd3c3
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-20 21:20:07 +0000

    Prints out all addresses with their cities and countries

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -2,10 +2,6 @@ select "addresses"."line1",
 "addresses"."district",
 "cities"."name" as "city",
 "countries"."name" as "country"
-from "customers"
-join "payments" using ("customerId")
-join "addresses" using ("addressId")
+from "addresses"
 join "cities" using ("cityId")
-join "countries" using ("countryId")
-order by "payments"."amount" desc
-limit 10;
+join "countries" using ("countryId");

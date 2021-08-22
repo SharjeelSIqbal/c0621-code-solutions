94ad28221e35c8c37157e9e7d3a1b4d3ef03a3f1
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-22 18:02:59 +0000

    CPut requests working as well as delete requests

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -11,19 +11,21 @@ const db = new pg.Pool({
     database: 'studentGradeTable'
   }
 });
+
 app.use(express.json());
+
 app.get('/api/grades', (req, res) => {
   const sql = `
   select *
   from "grades"
   `;
-  db.query(sql, (error, result) => {
-    if (!error) {
-      res.status(200).json(result.rows, null, 2);
-    } else {
-      res.status(400).json(error);
-    }
-  });
+  db.query(sql)
+    .then(results => {
+      res.status(200).json(results.rows, null, 2);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 app.post('/api/grades', (req, res) => {
@@ -33,133 +35,70 @@ app.post('/api/grades', (req, res) => {
   insert into "grades"
   ("name", "course", "score")
   values ($1, $2, $3)
-  returning "grades";
+  returning *;
   `;
   if (!newGrade.name || !newGrade.course || !newGrade.score) {
-    res.status(400).send('Bad request');
-  } else {
-    db.query(sql, values, (err, results) => {
-      if (err) {
-        res.status(500).send('An unexpected error occurred');
-      } else {
-        res.status(201).send(results.rows);
-      }
-    });
+    res.status(400).json('Bad request');
+    return;
   }
+  db.query(sql, values)
+    .then(results => {
+      res.status(201).json(results.rows[0], null, 2);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-// function getIdNumber (param){
-//   param = parseInt(param, 10);
-//   app.get('/api/grades/:gradeId'), (req, res){
-//     if(!Number.isInteger(param) || param <= 0){
-//       res.status(400).json({
-//         error: '"gradeId" must be positive integer'
-//       })
-//       return;
-//     }
-//     const sql = `select "gradeId" from "grades"`
+app.put('/api/grades/:gradeId', (req, res) => {
+  const updatedGrade = req.body;
+  const values = [updatedGrade.name, updatedGrade.course, updatedGrade.score, req.params.gradeId];
+  const sqlUpdate = `
+  update "grades"
+  set "name" = $1,
+      "course" = $2,
+      "score" = $3
+  where "gradeId" = $4
+  returning *;;
+  `;
+  if (!req.params.gradeId || req.params.gradeId < 0) {
+    res.status(400).json('Bad Request');
+    return;
+  }
+  db.query(sqlUpdate, values)
+    .then(results => {
+      if (results.rows.length > 0) {
+        res.status(200).json(results.rows[0]);
+      } else {
+        res.status(404).json(`${req.params.gradeId} does not exist`);
+      }
+    }).catch(err => {
+      res.status(500).json(err);
+    });
+});
 
-//     const params = [param];
-//     db.query(sql, params)
-//     .then(results => {
-//       const grade = results.rows[0];
-//       if(!grade){
-//         res.status(404).send(`Cannot find ${params[0]}`);
-//         return;
-//       } else {
-//       }
-//     })
-//     .catch(err)
-//   });
-// }
-
-// app.put('/api/grades/:gradeId'), (req, res) {
-
-// }
-
-// app.put('/api/grades/:gradeId', (req, res) => {
-//   // const sqlfindId = `
-//   //   select *
-//   //   from "grades"
-//   //   where "gradeId";
-//   //   `;
-//   //   const id = [];
-
-//   // db.query(sqlfindId, (err, res) => {
-//   //   if(err){
-//   //     res.status(500).send(err);
-//   //   } else {
-
-//   //   }
-//   // })
-//   const updatedGrade = req.body;
-//   const values = [updatedGrade.name, updatedGrade.course, updatedGrade.score, req.params.gradeId];
-//   const sql = `
-//   update "grades"
-//   set "name" = $1,
-//       "course" = $2,
-//       "score" = $3
-//   where "gradeId" = $4
-//   returning *;;
-//   `;
-//   if (!req.params.gradeId || req.params.gradeId < 0) {
-//     res.status(400).send('Bad Request');
-//     return;
-//   }
-//   db.query(sql, values, (err, results) => {
-//     if (err) {
-//       res.status(404).send(results.rows);
-//       return;
-//     }
-//     if (results.rows.length > 0) {
-//       res.status(200).send(results.rows);
-//       return;
-//     }
-//     res.status(500).send('Unexpected error occurred');
-//   });
-// });
-
-// app.post('/api/grades', (req, res) => {
-//   const newGrade = req.body;
-//   if (newGrade.name && newGrade.course && newGrade.score) {
-//     const sqlIdNumber = `
-//     select max("gradeId")
-//     from "grades"
-//     group by "gradeId"
-//     order by "gradeId" desc
-//     limit 1;
-//     `;
-//     db.query(sqlIdNumber, (err, result) => {
-//       if (err) {
-//         res.status(400).json(err);
-//       } else {
-//         newGrade.gradeId = result
-//         res.status(200).json(result);
-//       }
-//     });
-//     const sqlInsert = `
-//     insert into "grades"
-//     ("gradeId",
-//     "name",
-//     "course",
-//     "score",
-//     "createdAt")
-//     values ('${newGrade.gradeId}',
-//     '${newGrade.name}',
-//     '${newGrade.course}',
-//     '${newGrade.score}',
-//     '${newGrade.createdAt}')
-//     returning *;`;
-//     db.query(sqlInsert, (err, result) => {
-//       if (!err) {
-//         res.status(201).json(result.rows);
-//       } else {
-//         res.status(500).json(err);
-//       }
-//     });
-//   } else {
-//     res.status(400).json('Bad Request');
-//   }
-// });
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const values = [req.params.gradeId];
+  const sqlDelete = `
+  delete from "grades"
+  where "gradeId" = $1
+  returning *
+  `;
+  if (!req.params.gradeId || req.params.gradeId < 0) {
+    res.status(400).json('Bad Request');
+    return;
+  }
+  db.query(sqlDelete, values)
+    .then(results => {
+      if (results.rows.length > 0) {
+        res.sendStatus(204);
+      } else {
+        res.status(400).json(`${req.params.gradeId} does not exist`);
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 app.listen(3000);

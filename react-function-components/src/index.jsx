06cb0ff7cd2670da0582f58081cd843ead71b84e
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-24 19:36:14 +0000

    Changed function in to a react element

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 function CustomButton() {
   return <button>Click Me!</button>;
 }
-ReactDOM.render(CustomButton, document.getElementById('root'));
+ReactDOM.render(<CustomButton />, document.getElementById('root'));

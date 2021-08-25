f95e65112204e837112be1e27d59ee7bf25ee27c
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-25 19:36:25 +0000

    Only one property inside state, click and it increments with set state anytime the button is clicked

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -3,46 +3,31 @@ import React from 'react';
 class HotButton extends React.Component {
   constructor(props) {
     super(props);
-    this.count = 1;
     this.state = {
-      color: 'ice-age',
-      name: 'Ice'
+      count: 0
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    if (this.count < 3) {
-      this.setState({ color: 'ice-age' }, () => {
-        this.count += 1;
-      });
-    } else if (this.count < 6) {
-      this.setState({ color: 'cold', name: 'Cold' }, () => {
-        this.count += 1;
-      });
-    } else if (this.count < 9) {
-      this.setState({ color: 'room-temperature', name: 'Room Temp' }, () => {
-        this.count += 1;
-      });
-    } else if (this.count < 12) {
-      this.setState({ color: 'warm', name: 'Warm' }, () => {
-        this.count += 1;
-      });
-    } else if (this.count < 15) {
-      this.setState({ color: 'hot', name: 'Boiling' }, () => {
-        this.count += 1;
-      });
-    } else {
-      this.setState({ color: 'nuclear-fall-out', name: 'Death' });
-    }
+    this.setState({ count: this.state.count + 1 });
   }
 
   render() {
-    return (
-      <button onClick={this.handleClick} className={this.state.color}>{this.state.name}</button>
-    );
+    if (this.state.count < 3) {
+      return <button onClick={this.handleClick} className="ice-age">Hot Button</button>;
+    } else if (this.state.count < 6) {
+      return <button onClick={this.handleClick} className="cold">Hot Button</button>;
+    } else if (this.state.count < 9) {
+      return <button onClick={this.handleClick} className="room-temperature">Hot Button</button>;
+    } else if (this.state.count < 12) {
+      return <button onClick={this.handleClick} className="warm">Hot Button</button>;
+    } else if (this.state.count < 15) {
+      return <button onClick={this.handleClick} className="hot">Hot Button</button>;
+    } else {
+      return <button className="nuclear-fall-out">Hot Button</button>;
+    }
   }
-
 }
 
 export default HotButton;

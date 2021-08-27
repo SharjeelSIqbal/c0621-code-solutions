5911b56fa41439213265aa63709e2b1356eff732
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-27 23:43:50 +0000

    Added pictures and basic framework for the challenge, messed around with transitions as well

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: 0
+    };
+    this.nextImage = this.nextImage.bind(this);
+    this.timer = this.timer.bind(this);
+  }
+
+  nextImage() {
+    this.setState({ image: this.state.image + 1 });
+  }
+
+  timer(e) {
+    setInterval(this.nextImage, 1000);
+  }
+
+  render() {
+    return (
+      <div onLoadStart={this.timer}>
+        <img src={this.props.shrek[0].url} alt="" />
+      </div>
+    );
+  }
+}
+
+export default Carousel;

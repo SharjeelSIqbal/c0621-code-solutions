4e0863113dd0fdee1ed4bfaf0bb040d00bfd6243
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-08-29 03:26:06 +0000

    Work in progress, added basic components for clarity

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
+// import React from 'react';
 
-class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: 0
-    };
-    this.nextImage = this.nextImage.bind(this);
-    this.timer = this.timer.bind(this);
-  }
+// class Carousel extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       image: 0
+//     };
+//     this.nextImage = this.nextImage.bind(this);
+//     this.timer = this.timer.bind(this);
+//   }
 
-  nextImage() {
-    this.setState({ image: this.state.image + 1 });
-  }
+//   nextImage() {
+//     this.setState({ image: this.state.image + 1 } => {
+//       if(this.state.image === this.props.shrek.length){
+//         this.setState({image: 0})
+//       }
+//     });
+//   }
 
-  timer(e) {
-    setInterval(this.nextImage, 1000);
-  }
+//   timer(e) {
+//     setInterval(this.nextImage, 1000);
+//   }
 
-  render() {
-    return (
-      <div onLoadStart={this.timer}>
-        <img src={this.props.shrek[0].url} alt="" />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div onLoadStart={this.timer}>
+//         <img src={this.props.shrek[0].url} alt="" />
+//       </div>
+//     );
+//   }
+// }
 
-export default Carousel;
+// export default Carousel;

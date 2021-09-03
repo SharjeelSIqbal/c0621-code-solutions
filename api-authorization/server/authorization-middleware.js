135f61e03fcad0b1060a88f69329b966830761d0
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-09-03 02:30:12 +0000

    Added an err to the next so express can deal with it

--- api-authorization/server/authorization-middleware.js
+++ api-authorization/server/authorization-middleware.js
@@ -13,23 +13,9 @@ function authorizationMiddleware(req, res, next) {
     req.user = payload;
     next();
   } catch (err) {
-    console.error(err);
-    next();
+    next(err);
   }
-  /**
-   * Try to get the 'X-Access-Token' from the req.headers.
-   * If no token is provided,
-   *   throw a 401 error with the message 'authentication required'
-   * Use jwt.verify() to verify the authenticity of the token and extract its payload
-   * Assign the extracted payload to the user property of the req object.
-   * Call next() (with no arguments) to let Express know to advance to its next route or middleware.
-   */
 
-  /**
-    * References:
-    * https://nodejs.org/api/http.html#http_message_headers
-    * https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
-    */
 }
 
 module.exports = authorizationMiddleware;

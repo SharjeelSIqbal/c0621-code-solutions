e4e03679d0fe57a29e9ca5d7995dcab2ec8af008
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-09-22 16:59:38 +0000

    Pokeballs switch according to the current image with the setTimeout, but having a memory leak that crashes app whena pokeball is clicked

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -9,9 +9,14 @@ class Carousel extends React.Component {
     };
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
+    this.openPokeball = this.openPokeball.bind(this);
   }
 
-  next() {
+  componentDidMount() {
+    this.rotating = setInterval(this.next, 1000);
+  }
+
+  next(e) {
     if (this.state.counter < this.props.pokemon.length) {
       this.setState({ counter: this.state.counter + 1 }, () => {
         if (this.state.counter === this.props.pokemon.length) {
@@ -23,9 +28,12 @@ class Carousel extends React.Component {
         }
       });
     }
+    if (e) {
+      this.reset();
+    }
   }
 
-  prev() {
+  prev(e) {
     if (this.state.counter >= 0) {
       this.setState({ counter: this.state.counter - 1 }, () => {
         if (this.state.counter === -1) {
@@ -37,19 +45,46 @@ class Carousel extends React.Component {
         }
       });
     }
+    if (e) {
+      this.reset();
+    }
+  }
+
+  openPokeball(e) {
+    const counter = e.target.getAttribute('value');
+    this.setState({ image: this.props.pokemon[counter].url });
+  }
+
+  reset() {
+    clearInterval(this.rotating);
+    this.rotating = setInterval(this.next, 2000);
   }
 
   render() {
 
-    // const rotatidng = setInterval(this.next, 3000);
-
-    return (<div className="row true-center">
-      <div className="container row space-between">
-        <i className="fas fa-chevron-left"></i>
-      <img className="col-80" src={this.state.image} alt="" />
-        <i className="fas fa-chevron-right"></i>
+    return (
+    <>
+    <div className="container padding">
+    <div className="row true-center">
+      <div>
+        <div className="row space-between col-100">
+          <i onClick={this.prev} className="fas fa-chevron-left"></i>
+          <img className="col-80" src={this.state.image} alt="" />
+          <i onClick={this.next} className="fas fa-chevron-right"></i>
+        </div>
       </div>
-    </div>);
+      </div>
+      <div className="row true-center col-100">
+        {this.props.pokemon.map((element, index) => this.state.image === element.url
+          ? <img key={element.url} src={'../images/icons8-open-pokeball-48.png'} />
+          : <img key={element.url} onClick={this.openPokeball} value={index} src={'../images/icons8-pokeball-48.png'} />
+        )
+        }
+      </div>
+    </div>
+
+    </>
+    );
   }
 }
 
